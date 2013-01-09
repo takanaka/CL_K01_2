c649ceee398bb31bdb5b6c5d7cb3563f17dd1360
takanaka mutuki <bcmt1220027@gn.iwasaki.ac.jp>
2013-01-09 10:37:41 +0900

    文字化けの予防
    文字化け予防のために日本語を排除

--- CL1_K01_2.c
+++ CL1_K01_2.c
@@ -1,5 +1,4 @@
-/*作成者：１T１　（２３）　鷹中　睦基*/
-/*課題名：課題01_2*/
+
 #include<stdio.h>
 
 void main(void)	{
@@ -7,6 +6,6 @@ void main(void)	{
 	int width;
 	width = 5;
 
-	printf("一辺が%dの正方形の面積は%dです。",width,width*width);
+	printf("ippennga %dnoseihoukeino mennsekiha%d desu ",width,width*width);
 
 }
